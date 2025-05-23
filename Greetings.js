8ba94bc0c66c585bcba64992fb6eaea8427949ce
Shaik Amar <156987603+AmarShaik@users.noreply.github.com>
2025-05-23 19:06:26 +0000

    Add Greetings.js to print greeting

---
+++ Greetings.js
@@ -0,0 +1,11 @@
+// HackerRank - 10 Days of JavaScript: Hello, World
+// This function prints "Hello, World!" and the value of the input string passed to it.
+
+function greeting(parameterVariable) {
+    // Print the first line: "Hello, World!"
+    console.log('Hello, World!');
+
+    // Print the contents of parameterVariable (input string)
+    console.log(parameterVariable);
+}
+greeting("Welcome to JavaScript")

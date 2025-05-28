8ace2d3fc1798554da94f2ba60374ddcdfabe5ca
Shaik Amar <shaikamar907@gmail.com>
2025-05-28 16:46:47 +0000

    Add reversestring.js

---
+++ reversestring.js
@@ -0,0 +1,12 @@
+// reverse a string and use try and catch
+function reverseString(s) {
+    try{
+        let split_string = s.split('');
+        split_string.reverse();
+        console.log(split_string.join(''));
+    }
+    catch(e){
+        console.log(e.message);
+        console.log(s);
+    }
+}
